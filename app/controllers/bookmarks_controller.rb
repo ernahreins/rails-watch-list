dc5f119e3c3c11c508afa1f611920c123e2b282c
ErnaHreinsdottir <erna.hreins@gmail.com>
2022-02-17 14:51:02 +0100

    half way

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,19 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @list = List.new(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+
+end
